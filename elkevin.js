98489c27413c1d74b3833f18d51be4136b98f7a6
lgaieta <lgaieta.dev@gmail.com>
2024-04-24 14:56:13 -0300

    Added video to canvas

--- elkevin.js
+++ elkevin.js
@@ -1,8 +1,13 @@
 function setup() {
+  createCanvas(800, 600);
 
+  let video = createVideo("/video1.mp4");
+
+  video.position(0, 0);
+  video.size(800, 600);
+  video.play();
 }
 
-
 function draw() {
-
+  background(230);
 }
