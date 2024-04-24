afb30d65c1520a7c8c59d0a8fe40148c393d2b6f
lgaieta <lgaieta.dev@gmail.com>
2024-04-24 15:48:35 -0300

    Added ellipse when video ends

--- elkevin.js
+++ elkevin.js
@@ -1,13 +1,22 @@
+let isDone = false;
+
 function setup() {
   createCanvas(800, 600);
-
-  let video = createVideo("/video1.mp4");
+  let video = createVideo("./video1.mp4", () => console.log("loaded video"));
 
   video.position(0, 0);
   video.size(800, 600);
-  video.play();
+  video.autoplay();
+  video.time(55);
+
+  video.onended(() => {
+    video.hide();
+    isDone = true;
+  });
 }
 
 function draw() {
   background(230);
+
+  if (isDone) ellipse(width * 0.5, height * 0.5, 40, 40);
 }
