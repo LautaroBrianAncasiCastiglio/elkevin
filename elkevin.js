b414d9a50a768610211c08d9c3d99f29e50e30b9
LautaroBrianAncasiCastiglio <lautarocas05@gmail.com>
2024-04-27 19:20:11 -0300

    se añadio una clase para los videos

--- elkevin.js
+++ elkevin.js
@@ -1,38 +1,65 @@
 let isDone = false;
 
+class Videos{
+  //aqui se declara el nombre del 
+  //video(para reconocer el tipo de video que hablamos)
+  //y el link de este video y si se visualiza(1) o no(0)
+  constructor(name, link, visualizar){
+    this.name =name;
+    this.link = createVideo(link, function(){console.log("video cargado")});
+    this.visualizar = visualizar;
+  }
+  //esta sera la posicion de los videos
+  posicion(){
+    this.x = 440;
+    this.y = 550;
+  }
+  //aca se muestra el video
+  show(){
+    if(this.visualizar == 1){
+    this.link.position(0, 0);
+    this.link.size(800, 600);
+    this.link.time(10);
+    this.link.play();
+    this.link.time(55);
+    this.visualizar = 0;}
+  }
+
+}
+
+
 function setup() {
   createCanvas(800, 600);
-  let video = createVideo("./video1.mp4", () => console.log("loaded video"));
+  cargaVideos();
+  /*let video = createVideo("./video1.mp4", () => console.log("loaded video"));
 
   video.position(0, 0);
   video.size(800, 600);
-  video.autoplay();
+  video.play();
   video.time(55);
 
   video.onended(() => {
     video.hide();
     isDone = true;
-  });
+  });*/
 }
 //es una variable de confirmacion
 let val=2;
+
 function draw() {
   background(230);
 
-  if (isDone) {ellipse(width * 0.5, height * 0.5, 40, 40);
+  /*if (isDone) {ellipse(width * 0.5, height * 0.5, 40, 40);
     if(val == 2){
       //cambia su valor a 1
     val = 1;}
-  }
-  //si la variable es 1 entonces que reproduzca el video
-  if(val == 1){
-    let video2 = createVideo("./video1.mp4", () => console.log("loaded video"));
-    video2.position(0, 0);
-    video2.size(800, 600);
-    video2.time(10);
-    video2.play();
-    ellipse(10,10,10,10);
-    //deja de ser 1 y no se reproduce el video
-    val = 0;
-  }
+  }*/
+  
+  videos.show();
+
+
+}
+
+function cargaVideos(){
+  videos = new Videos("1", "./video1.mp4","1");
 }
